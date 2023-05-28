e11a193983389d6a069c0608cd04964737b35a67
sumit <abc@gmail.com>
2023-05-28 22:00:18 +0530

    changing port

--- server.js
+++ server.js
@@ -152,5 +152,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
